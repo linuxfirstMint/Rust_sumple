3540c80abd73f9ab3b4b43423e4b7c9d60486ba4
flm_yk012 <firstlinuxmint@gmail.com>
2020-07-06 22:49:33 +0900

    追加 複数回解答できるループ機能

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,23 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is {}", secret_number);
+    loop {
+        println!("The secret number is {}", secret_number);
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    // コロン ":" でコンパイラへ型注釈を行う
-    // guessに含まれるホワイトスペース(改行コード"\n"含む)を削除し別の型へ変換
-    // 成功したらOk列挙子(自分自身=u32型のguess）を返す
-    // 失敗したらErr列挙子(Panicと引数のメッセージ)を返す
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
