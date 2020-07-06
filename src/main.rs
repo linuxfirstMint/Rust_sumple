7aa993f27d69e1d7d99b69cf7ebc08c7969a4de2
flm_yk012 <firstlinuxmint@gmail.com>
2020-07-06 22:37:05 +0900

    追加 ユーザ入力と乱数を比較する機能

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-extern crate rand; // rand::が使えるようになる
+extern crate rand;
 
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -17,5 +18,17 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    // コロン ":" でコンパイラへ型注釈を行う
+    // guessに含まれるホワイトスペース(改行コード"\n"含む)を削除し別の型へ変換
+    // 成功したらOk列挙子(自分自身=u32型のguess）を返す
+    // 失敗したらErr列挙子(Panicと引数のメッセージ)を返す
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
